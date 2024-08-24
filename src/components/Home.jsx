cdb3651d7395fecf68a10cdff9af46ae55a34c57
JoshuaFigueroaCaguimbay <joshua.figueroacaguimbay@gmail.com>
2024-08-24 16:10:25 -0400

    Added brief introduction and Button to club sign up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap"; 
+import "../styles/home.css";
 
 export default function Home(){
     return (
         <div>
             <Container>
-                <h1>home</h1>
+                <h1 className="welcome">WELCOME TO WCI COMPUTER SCIENCE CLUB</h1> 
+                <h2>We are a group of Weston Students who work together on Coding projects!</h2> 
+                <button className="join-button" onclick="window.location.href='https://hackclub.com/';">JOIN OUR CLUB!</button>
             </Container>
         </div>
     );
