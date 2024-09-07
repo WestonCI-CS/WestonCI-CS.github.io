d3f04dcf4121655e128e72c1fbb8c38be2eddc4f
vunguyen312 <vunguyen312@outlook.com>
2024-09-06 21:46:19 -0400

    Fixed broken pfp links and added images folder

--- src/pages/Members.jsx
+++ src/pages/Members.jsx
@@ -3,9 +3,13 @@ import MemberCard from "../components/MemberCard";
 import members from "../json/members.json";
 
 export default function Members(){
+    const getPFP = (file) => {
+        return require(`../assets/images/pfp/${file}`);
+    }
+
     const memberData = members.map(member => (
         <MemberCard
-            image={member.image}
+            image={getPFP(member.image)}
             name={member.name}
             style={{backgroundImage: `linear-gradient(120deg, ${member.gradientStart} 0%, ${member.gradientEnd} 100%)`}}
             role={member.role}
